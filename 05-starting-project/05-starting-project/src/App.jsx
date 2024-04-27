f49da0879cd1eafc013b4ea6e09a009f6f438c7f
parknari02 <pnr021118@naver.com>
2024-04-27 15:49:43 +0900

    연습프로젝트 완료

--- 05-starting-project/05-starting-project/src/App.jsx
+++ 05-starting-project/05-starting-project/src/App.jsx
@@ -12,20 +12,25 @@ function App() {
     duration: 10,
 });
 
+const inputIsValid = userInput.duration >= 1;
+
 function handleChange(inputIdentifier, newValue) {
   setUserInput(prevUserInput => {
       return {
           ...prevUserInput,
-          [inputIdentifier]: newValue
+          [inputIdentifier]: +newValue,
       };
   });
 }
+
+
 
   return (
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput}/>
+      {!inputIsValid && <p className="center">Please enter a duration greater than zero.</p>}
+      {inputIsValid && <Results input={userInput}/>}
     </>
   );
 }
